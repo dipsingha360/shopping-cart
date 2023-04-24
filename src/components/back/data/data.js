d8c8c730eedc2806ef9098909d6db197bf419382
Dip Singha <dipsingha2@gmail.com>
2023-04-25 01:06:31 +0600

    shopping cart project added

---
+++ src/components/back/data/data.js
@@ -0,0 +1,48 @@
+const data = {
+  productItems: [
+    {
+      id: "1",
+      name: "Beats Headphone",
+      price: 1000,
+      image:
+        "https://images.ctfassets.net/16nm6vz43ids/46o2J8l3VuvOTfowzrWchN/fcfb1d6e9f1882b7e249dd82de0bf60b/How_to_back_up_your_phone.png",
+    },
+    {
+      id: "2",
+      name: "QKZ DM10 wired earphone",
+      price: 350,
+      image:
+        "https://images.ctfassets.net/16nm6vz43ids/46o2J8l3VuvOTfowzrWchN/fcfb1d6e9f1882b7e249dd82de0bf60b/How_to_back_up_your_phone.png",
+    },
+    {
+      id: "3",
+      name: "Asus vivobook 450",
+      price: 100000,
+      image:
+        "https://images.ctfassets.net/16nm6vz43ids/46o2J8l3VuvOTfowzrWchN/fcfb1d6e9f1882b7e249dd82de0bf60b/How_to_back_up_your_phone.png",
+    },
+    {
+      id: "4",
+      name: "Realmi 8",
+      price: 22000,
+      image:
+        "https://images.ctfassets.net/16nm6vz43ids/46o2J8l3VuvOTfowzrWchN/fcfb1d6e9f1882b7e249dd82de0bf60b/How_to_back_up_your_phone.png",
+    },
+    {
+      id: "5",
+      name: "Realmi 8",
+      price: 21000,
+      image:
+        "https://images.ctfassets.net/16nm6vz43ids/46o2J8l3VuvOTfowzrWchN/fcfb1d6e9f1882b7e249dd82de0bf60b/How_to_back_up_your_phone.png",
+    },
+    {
+      id: "6",
+      name: "Realmi 8",
+      price: 24000,
+      image:
+        "https://images.ctfassets.net/16nm6vz43ids/46o2J8l3VuvOTfowzrWchN/fcfb1d6e9f1882b7e249dd82de0bf60b/How_to_back_up_your_phone.png",
+    },
+  ],
+};
+
+export default data;
